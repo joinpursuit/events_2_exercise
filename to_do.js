991ed8d1becb6f5753272405fe84d70611a5376a
Pixelynx <tjstevens1992@gmail.com>
2018-11-12 17:15:55 -0500

    started

---
+++ to_do.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // form input from user adds new li element at bottom of ul
+    // pressing submit creates new li element; appends to end of ul
+
+  let to_do = document.querySelector('.to_do');
+  let newItem = document.createElement('li');
+
+  to_do.appendChild()
+
+  // if user writes nothing; page shows error message
+    // pressing submit when nothing is entered
+
+  // when user clicks on li; item strikes out
+    // pressing submit updates style of li to strikethrough
+
+  // page does not refresh
+
+
+
+
+});
