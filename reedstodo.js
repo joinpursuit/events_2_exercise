26c4ff1aed27190643222a5a0ec35c0e86bc182c
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2018-11-12 21:37:44 -0500

    Evgeniya Ezhova

--- reedstodo.js
+++ reedstodo.js
@@ -5,18 +5,20 @@ let ul = document.getElementById("unordered-list")
 let userInput = document.getElementById("userInput");
 let button = document.getElementById("button");
 let form = document.getElementById("form")
+let h4 = document.getElementById("err4")
 
 button.addEventListener("click", event => {
   event.preventDefault();
+  h4.innerHTML = " "
   let newNode = document.createElement("li")
   newNode.innerText = userInput.value;
-  if(userInput.value.length > 0){
+  let errormsg = document.createElement("p");
+  errormsg.innerText = "Error! Please enter valid text";
+  if(userInput.value.length === 0){
+    h4.appendChild(errormsg);
+  } else {
     ul.appendChild(newNode);
     form.reset();
-  } else {
-    let errormsg = document.createElement("p");
-    errormsg.innerText = "Error! Please enter valid text";
-    form.appendChild(errormsg)
   }
 });
 
