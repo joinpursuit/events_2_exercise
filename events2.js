e077ffa62f3aa846baa93cc151137646d9e8c008
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-13 18:33:13 -0500

    5th commit

--- events2.js
+++ events2.js
@@ -1,101 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-//
-// //--------------------------------
-// // function error() {
     let form = document.querySelector(".form")
 //
-//     form.addEventListener("submit", event => {
-//       event.preventDefault();
-//
-//       let textInput = document.querySelector(".input").innerText;
-//       let newP = document.createElement("p");
-//
-//       newP.innerText = "Error";
-//       event.currentTarget.appendChild(newP)
-//     })
+    form.addEventListener("submit", event => {
+      event.preventDefault();
+      //---------------------------
+      // if there is no inner text:
+      // //--------------------------------
+      let textInput = document.querySelector(".input").innerText;
+      let newP = document.createElement("p");
 
-// }
-//--------------------------------
+      newP.innerText = "Error";
+      event.currentTarget.appendChild(newP)
+
+      //--------------------------------
+      //if there is inner text:
+      //---------------------------
+      // console.log("textInput", textInput);
+      // console.log("theUl", theUl);
+
+      let textInput; = document.querySelector(".input").innerText;
+
+      if (!textInput) {
+
+        form.addEventListener("submit", event => {
+        event.preventDefault();
+
+        let input = document.querySelector(".input");
+        let textInput= input.value;
+        let ul = document.querySelector("ul");
+
+    // console.log("theUl", theUl);
+
+        let newLi = document.createElement("li");
+        console.log(input);
+        console.log(textInput);
+        // let innerLi = newLi.innerText;
+
+        newLi.innerText = textInput;
+        ul.appendChild(newLi)
+        // console.log("newLi", newLi);
+
+        // console.log("", textInput);
+        })
+
+      } else {
 
 
+        //-----------------------------
+        // cross out the done "to do":
+        //-------------------------------
 
-//---------------------
-// if statement there is something in input : ----------------------
-  // let textInput = document.querySelector(".input").innerText;
-  //
-  // if (textInput=== "undefined") {
-  //   error()
-  // }
+        // let body = document.querySelector("body");
+        // body.addEventListener("click", event => {
 
-//-----------------------------
+        let findLi = document.querySelectorAll("li");
+        console.log("findLi", findLi);
 
-
-
-
-//---------------------------
-  // console.log("textInput", textInput);
-  // console.log("theUl", theUl);
-
-  form.addEventListener("submit", event => {
-    event.preventDefault();
-
-    let input = document.querySelector(".input");
-    let textInput= input.value;
-    let ul = document.querySelector("ul");
-
-// console.log("theUl", theUl);
-
-    let newLi = document.createElement("li");
-    console.log(input);
-    console.log(textInput);
-    // let innerLi = newLi.innerText;
-
-    newLi.innerText = textInput;
-    ul.appendChild(newLi)
-    // console.log("newLi", newLi);
-
-    // console.log("", textInput);
+        findLi.forEach(elem => {
+          findLi.addEventListener("click", event => {
+            event.target.style.textDecoration = "line-through"
+          })
+        })
+      }
     })
-
-//-------------------------------
-
-// let body = document.querySelector("body");
-// body.addEventListener("click", event => {
-
-  let findLi = document.querySelectorAll("li");
-  console.log("findLi", findLi);
-
-  findLi.forEach(elem => {
-    findLi.addEventListener("click", event => {
-      event.target.style.textDecoration = "line-through"
-    })
-  })
-// })
-
-//----------------------------
-
-
-
-
-
-
-
-
-
-
-//scrap:-----
-    // let textInput = querySelector(".input")
-    // console.log(textInput);
-    // // makeLI.innerText = "dka djaflk;a";
-    // // event.target.parentNode.replaceChild
-//end of scrap:------
-
-
-//
-
-
-
 
 
 })
