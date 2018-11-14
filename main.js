2595b13a2b54f670431720276c2a748963c19168
mateo-navarrete <mateonavarrete@pursuit.org>
2018-11-14 14:13:03 -0500

    updated styles

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const clearInputAddTodo = (todo) => {
   let li = document.createElement('li');
   // li.classList.add('todo');
   let todoSlice = todo.length > 20 ? todo.slice(0, 20) : todo;
+  // let todoSlice = todo;
   li.innerHTML = `<p class='todo'>${todoSlice}</p>`;
   // li.style.backgroundColor = getColor();
   ul.appendChild(li);
@@ -64,6 +65,7 @@ const errMsg = {
     this.active = true;
     let form = document.querySelector('form');
     let p = document.createElement('p');
+    p.classList.add('err');
     p.innerText = `Please enter a valid todo!`;
     form.appendChild(p);
   },
