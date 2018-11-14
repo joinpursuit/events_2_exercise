08f7415c635f030274fbf2ea3c7fa8b09f6d1679
mateo-navarrete <mateonavarrete@pursuit.org>
2018-11-13 21:51:03 -0500

    added clearError & clearInputAddTodo

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
-let date = Date();
-console.log('loaded', date);
-
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
   let form = document.querySelector('form');
-  form.addEventListener('submit', (e)=> {
+  let input = document.querySelector('input');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(e);
+    let todo = input.value.trim();
+    todo ? clearInputAddTodo(todo) : clearError();
+    // console.log(input, input.value);
+    //console.log(e, e.target);
   });
 });
+
+const clearInputAddTodo = (todo) => {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerText = `${todo}`;
+  ul.appendChild(li);
+  document.querySelector('input').value = '';
+  if (errMsg.active) {
+    errMsg.clear();
+  }
+};
+
+const clearError = () => {
+  document.querySelector('input').value = '';
+  if (!errMsg.active) {
+    errMsg.display();
+  }
+};
+
+const errMsg = {
+  active: false,
+  clear: function () {
+    this.active = false;
+    document.querySelector('p').remove();
+  },
+
+  display: function () {
+    this.active = true;
+    let form = document.querySelector('form');
+    let p = document.createElement('p');
+    p.innerText = `Please enter a valid todo!`;
+    form.appendChild(p);
+  },
+};
