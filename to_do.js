47e548174efb58206acaa2dd6790d7c20d2bb69d
Treagan Birbal <treaganbirbal@pursuit.org>
2018-11-18 17:26:14 -0500

    Treagan Birbal

--- to_do.js
+++ to_do.js
@@ -1,22 +1,43 @@
-const list = document.querySelector('.uList');
-debugger
-// delete item
-// list.addEventListener('click', (event) => {
-//   if(e.target.className == 'delete'){
-//     const li = e.target.parentElement;
-//     li.parentNode.removeChild(li);
-//   }
-// });
+document.addEventListener("DOMContentLoaded",()=>{
+  let list = document.querySelector(".list")
+  let form = document.querySelector(".form")
+  let enterItem = document.querySelector(".enterItem")
+  let btn = document.querySelector(".btn")
+  let error = document.querySelector(".error")
 
-const forms = document.querySelector('.form');
-debugger
-Array(...forms).forEach(forms) => {
-  console.log(forms);
-});
-debugger
-const addForm = forms['form'];
-document.addEventListener('submit', function(event){
-  event.preventDefault();
-  const value = document.querySelector('input[type="text"]').value;
-  console.log(value);
-});
+  btn.addEventListener("click", () => {
+    let newItem = document.createElement('li')
+    newItem.innerText = enterItem.value;
+    list.appendChild(newItem);
+    // form.reset();
+    if(enterItem.value === true){
+
+    }
+    else if(enterItem.value === ""){
+      let alertError = error.innerText = "Alert: Please Add Something to Your To-Do List";
+
+      form.appendChild(error)
+
+    }
+    form.reset()
+
+
+  })
+})
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//
+// const item = () => {
+//   let newItem = document.createElement("li");
+//   newItem.innerText = input.value;
+//   list.appendChild(newItem);
+//   form.reset()
+//     }
+//   const errorMessage = () => {
+//     let error = document.querySelector("p");
+//     if(input.value.length < 1){
+//       let throwErr = error.innerHTML = "Please Add Something to the List"
+//       form.appendChild(error)
+//     }
+//   }
