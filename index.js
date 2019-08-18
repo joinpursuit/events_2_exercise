f2397db7837c1b145be74582e0130e0ce2a6e04a
Suzette <suzetteislam@gmail.com>
2019-08-18 11:52:48 -0400

    edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',() =>{
   let inputBox = document.querySelector('#inputBox'); //
-  let toDoList = document.querySelector('#toDoList'); // grabbing the ul because you only need it once.
+  let toDoList = document.querySelector('#toDoList'); // grabbing the ul because you will be re-using separately.
 
   let form = document.querySelector('form');
   // console.log(form)
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded',() =>{
       let inputValue = inputBox.value.trim();
       listItem.innerText = inputValue;
 
+    // can slow the page down if we have a lot of tasks
+      // listItem.addEventListener('click', () =>{
+      //   listItem.style.textDecoration = 'line-through';
+      // })
+
+
       let p = document.querySelector('#error') // have to create the variable
       if(inputValue !== ""){
         // adding item to the lists
@@ -22,4 +28,10 @@ document.addEventListener('DOMContentLoaded',() =>{
         p.innerText = "Error! Input cannot be empty."; // leaves an error message
       }
   })
+
+    toDoList.addEventListener('click', ()=>{
+      event.target.style.textDecoration = 'line-through';
+      console.log('the list was clicked.')
+      console.log(event.target)
+    })
 })
