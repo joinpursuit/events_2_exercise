cb36b1510ae803544fde5f3310971c6ef070d04f
Owen Jones <owenthefirst93@gmail.com>
2019-08-18 11:55:35 -0400

    finished

--- todoList.js
+++ todoList.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded',() =>{
     let form = document.querySelector('#form')
+    let grabInput = document.querySelector('#todotext')
+    let grabList = document.querySelector('#todolist')
+
      form.addEventListener('submit',(event)=> {
-         event.preventDefault()
-         let grabInput = document.querySelector('#todotext')
+         event.preventDefault()     
          console.log(grabInput.value)
          let makeErrorMessage = document.querySelector('#para')
-
+         let makeTodo = document.createElement('li')
+        
          if (grabInput.value === ''){
             makeErrorMessage.innerText = "Error: Todo cannot be empty"
-            
-            
-         } else {
-            let makeTodo = document.createElement('li')
-             let grabList = document.querySelector('#todolist')
+               
+         } else { 
             makeTodo.innerText = grabInput.value
              grabList.appendChild(makeTodo)
-         }        
-     })
+             makeErrorMessage.innerText = ''
+             grabInput.value = ''
+         }     
+        
+        grabList.addEventListener('click', (event)=>{
+            let li = event.target
+            li.style.textDecoration = 'line-through'
+        })      
+     })    
+           
     })
      
 
 
-    
+   
