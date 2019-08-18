7f01e4510ee980624ffa49e20cd3bc3bacb752e2
shaynadavis <shaynadavis@pursuit.org>
2019-08-18 05:55:42 -0400

    did not go as planned

--- Events_lab.js
+++ Events_lab.js
@@ -1,20 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
 
+    let myForm = document.querySelector('#form');
+    console.log(myForm)
+    myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+        window.alert('this shows the event')
+    })
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    let taskItemList = document.querySelectorAll('li');
+    
+    taskItemList.addEventListener('click', (event) => {
+        console.log('todos were clicked')
+    })
+})
 
 const addNewTask = () => {
     let taskInput = document.getElementById('new_task').value
     let taskItem = document.createElement('li')
     taskItem.innerText = taskInput
     let ul = document.getElementById('listCategory')
+    let form = document.getElementById("new_todo")
     ul.appendChild(taskItem)
-
-        if (taskItem.innerText === null) {
-        let errorMessage = document.createElement('p')
-        errorMessage.innerText = "Error Please Enter A Task"
-        document.body.appendChild("errorMessage")
-    } 
 }
 
 
-// //
+//why does my addeventListener keep showing up as null.
 
