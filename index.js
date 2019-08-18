85e6c4ed05ff60645f32d0a0271b4e53ecc3a736
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-08-17 21:20:37 -0400

    base level lab complete. moving on to bonus.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let strInputElement = document.querySelector("#text_input")
-    let userSubmitStr = strInputElement.value
-    let newListItem = document.createElement("li")
+
     let form = document.querySelector("#chaz")
-    console.log(strInputElement)
+
     form.addEventListener("submit", (event) => {
-      newListItem.innerText = userSubmitStr
-      listyMcListface.appendChild(newListItem)
-      event.preventDefault()
-      console.log(userSubmitStr)
+      let userInput = document.querySelector("#text_input").value
+      if (userInput === "") {
+        event.preventDefault()
+        let alert = document.querySelector("#error")
+        alert.innerText = "No text has been entered!"
+      } else {
+        event.preventDefault()
+        let newListItem = document.createElement("li")
+        newListItem.innerText = userInput
+        listyMcListface.appendChild(newListItem)
+      }
+
     })
 })
