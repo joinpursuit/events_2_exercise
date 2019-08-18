7139629d84f65172438cb800cca7197c4812565f
karenm09 <karenm929@hotmail.com>
2019-08-18 00:20:41 -0400

    completed

--- events_lab.js
+++ events_lab.js
@@ -3,17 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('submit', (event) => {
     event.preventDefault()
   let inputTask = document.getElementById("tasks").value
-  let emptyStr = document.getElementById("tasks").value
-    if (emptyStr === "") {
-    window.alert('Error. The list cannot be empty')
+  let para = document.getElementById("p1")
+    if (inputTask) {
+      let taskList = document.getElementById('to_do_list');
+      let newTask = document.createElement("li")
+      newTask.innerText = inputTask
+      taskList.addEventListener('click', (event) => {
+      event.target.style.textDecoration = "line-through";
+      })
+      to_do_list.appendChild(newTask)
+      para.innerText = ''
+      inputTask = ''
+    } else {
+      para.innerText = 'Error. The list cannot be empty'
     }
-  let newTask = document.createElement("li")
-  newTask.innerText = inputTask
-  to_do_list.appendChild(newTask)
-  })
-  let taskList = document.getElementById('to_do_list');
 
-  taskList.addEventListener('click', event => {
-  newTask.style.textDecoration = "line-through";
   })
 })
