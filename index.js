29b1c064de1adf4c2060ed66d096af0620027d56
Ohidurr <Ohidurrahman96@gmail.com>
2019-11-12 21:20:42 -0500

    parts 1 and 2 for events lab

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  let ul = document.querySelector("ul")
+ let submit = document.querySelector("#submit")
+  submit.addEventListener("click",(e) => {
+   e.preventDefault()
+   if(document.querySelector("#input").value === "" ){
+     document.querySelector("#null").innerText = "Error TODO cannot be empty"
+   } else{
+   let li = document.createElement("li")
+   li.innerText = document.querySelector("#input").value
+   ul.appendChild(li)
+ }
+
+ })
+ document.body.appendChild(ul)
+})
