09e6a69b3ff7fa68bd8941d9cdc853bd76806c39
John Jones Jr <jayjohnjones@gmail.com>
2019-11-13 11:07:34 -0500

    List updating

--- eventLab.js
+++ eventLab.js
@@ -1,19 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // let button = document.querySelector("#button")
-    // button.addEventListener("click", () => {
-    let form = document.querySelector("#form");
-    form.addEventListener("submit", () => {
-
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        
         let userInput = document.querySelector("#userInput")
         let input = userInput.value
         let ul = document.querySelector("#ul");
         let li = document.createElement("li");
         li.innerText = input
-        ul.appendChild(li);
-        document.body.appendChild(ul)
-    })
-    // let form = document.querySelector("#form");
-    // form.addEventListener("")
+        let error = document.createElement("p");//Creates a paragraph
 
+
+        if(input === ""){
+            error.innerText = 'Error. Todo cannot be empty' 
+            form.appendChild(error)
+            e.preventDefault();  // Alows the ul to populate with the li and not refresh.
+        
+        } else {
+            ul.appendChild(li);
+            e.preventDefault(); // Alows the ul to populate with the li and not refresh.
+        }
+    })
+   
 })
