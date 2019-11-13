d2cf7329991403a559d3d6006df43096eccaf999
John Jones Jr <jayjohnjones@gmail.com>
2019-11-13 11:23:53 -0500

    Place line on lab

--- eventLab.js
+++ eventLab.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
+    let form = document.querySelector("form"); // grabs the form
+    form.addEventListener("submit", (e) => { // event listner on the form's submit button
         
         let userInput = document.querySelector("#userInput")
         let input = userInput.value
@@ -11,15 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
         let error = document.createElement("p");//Creates a paragraph
 
 
-        if(input === ""){
+        if(input === ""){  // if the input is empty strings 
             error.innerText = 'Error. Todo cannot be empty' 
-            form.appendChild(error)
+            form.appendChild(error) //place error to the form
             e.preventDefault();  // Alows the ul to populate with the li and not refresh.
         
         } else {
-            ul.appendChild(li);
+            ul.appendChild(li); // places the li to the ul which is on the form
             e.preventDefault(); // Alows the ul to populate with the li and not refresh.
         }
+
+        li.addEventListener("click", () => { // listens for when a li is clicked
+            li.style.textDecoration = "line-through"; //Places a line through the li that was clicked
+            // debugger
+    
+        })
     })
+   
    
 })
