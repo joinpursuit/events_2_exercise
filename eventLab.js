3114f225f5142df450c7e825fd2c12c093f150c3
crystal cardona <crystalcardona@pursuit.org>
2019-11-12 21:59:09 -0500

    Pretty 1

--- eventLab.js
+++ eventLab.js
@@ -1,27 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    let form = document.querySelector("form")
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-        let li = document.createElement("li")
-        let task = document.querySelector("#task")
-        let ul = document.querySelector("#list")
-        let newTask = task.value
-        let p = document.createElement("p")
-        if(newTask === ""){
-            p.innerText = "Error no input"
-            document.body.appendChild(p)
-        } else {
-    
-            li.innerText = newTask
-            ul.appendChild(li)
-        }
-    })
-    ul.addEventListener("click", (e) => {
-        let strike = document.createElement("strike")
-        strike.innerText = e.currentTarget.value
-        
-        
-    })
-
-})
+  let form = document.querySelector("form");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    let li = document.createElement("li");
+    let task = document.querySelector("#task");
+    let ul = document.querySelector("#list");
+    let newTask = task.value;
+    let p = document.createElement("p");
+    if (newTask === "") {
+      p.innerText = "Error no input";
+      document.body.appendChild(p);
+    } else {
+      li.innerText = newTask;
+      ul.appendChild(li);
+    }
+    ul.addEventListener("click", e => {
+      let strike = document.createElement("strike");
+      strike.innerText = e.target;
+      ul.appendChild(strike);
+    });
+  });
+});
