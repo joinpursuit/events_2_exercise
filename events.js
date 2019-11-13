f69bd54ae4ef14acc75f2b904b7a0ca3a5a64631
Henrynunez112 <henrynunez@pursuit.org>
2019-11-13 17:06:27 -0500

    got an alert

--- events.js
+++ events.js
@@ -2,24 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     let userInput = {
         list: ""   
     }
+
     let form = document.querySelector("form")
     form.addEventListener("change", (e) =>{
-        userInput[e.target.id] = e.target.value
-        console.log(userInput)
+    userInput[e.target.id] = e.target.value
+    console.log(userInput)
     })
+
     form.addEventListener("submit", (e) =>{
-        e.preventDefault()
-        let li = document.createElement("li")
-        li.innerText = userInput["list"]
-        document.body.appendChild(li)
-        document.getElementById("list").value="";
-    })
-    let p = document.querySelector("p")
-    p.addEventListener("click", (e) => {
-        e.preventDefault()
-        if(userInput["list"] === ""){
-            e.id.innerText = "please put something in the text box"
-        }
+    e.preventDefault()
+    let li = document.createElement("li")
+    li.innerText = userInput["list"]
+    document.body.appendChild(li)
+    document.getElementById("list").value="";
+    
+    let p = document.querySelector("#emptyAlert")
+    if(userInput["list"] === ""){
+        p.innerText = "please put something in the text box"
+    }
     })
 
 })
