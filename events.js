fec387c38f0ce2e8e1299b38e0897f30246986da
Henrynunez112 <henrynunez@pursuit.org>
2019-11-12 21:50:55 -0500

    got the list going

--- events.js
+++ events.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let userInput = {
+        list: ""   
+    }
+    let form = document.querySelector("form")
+    form.addEventListener("change", (e) =>{
+        userInput[e.target.id] = e.target.value
+        console.log(userInput)
+    })
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault()
+        let li = document.createElement("li")
+        li.innerText = userInput["list"]
+        document.body.appendChild(li)
+
+        
+    })
+    let p = document.querySelector("nada")
+    p.addEventListener()
 
 })
-
-
-    const toDoList = () =>{
-        let userInput = document.querySelector("#list")
-        let string = userInput.value
-        let ul = document.querySelector("#newList")
-        let li = document.createElement("li")
-        li.innerText = string
-        ul.appendChild(li)
-       
-    }
