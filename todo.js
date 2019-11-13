20172c8ca322c7a663e56fb6cf589df844f4bb14
Kelvin <kelvinarellano@pursuit.org>
2019-11-12 21:50:06 -0500

    event created

---
+++ todo.js
@@ -0,0 +1,14 @@
+document.addEventListener("DomContentLoaded", ()=>{
+    let button1 = button.querySelector("button");
+    button1.addEventListener("click", addToList)
+
+    let toDoList = document.querySelector("#todo");
+
+    // let button2 = button.document.getElementById("bigger button")
+})
+
+
+toDoList.addEventListener(e)+>{
+    let newItem = document.createElement("item");
+    newItem.innerText= newItem.value
+}
