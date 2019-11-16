2a1e459c71a94b356331f6784f61771fab282081
bsihame <sihamebazi@pursuit.org>
2019-11-16 09:41:06 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   let h1 = document.createElement("h1");
-  let _form = document.createElement("form");
+  let form = document.createElement("form");
   let ul = document.createElement("ul");
   let userInput = document.createElement("input");
+  let para = document.createElement("p")
   let submitButton = document.createElement("button");
   
   userInput.placeholder = "Type a todo";
@@ -12,12 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   h1.innerText = "My To-Dos"
   document.body.appendChild(h1);
-  document.body.appendChild(_form);
-  _form.appendChild(ul);
-  _form.appendChild(userInput);
-  _form.appendChild(submitButton);
-  submitButton.addEventListener("click",()=>{
+  document.body.appendChild(form);
+  document.body.appendChild(para)
+  document.body.appendChild(ul);
+  form.appendChild(userInput);
+  form.appendChild(submitButton);
+
+  form.addEventListener("submit",()=>{
+    event.preventDefault()
+    let listItem = document.createElement("li")
+    if(userInput.value.trim() === ""){
+      para.innerText = "Please enter text"
+    } else {
+      listItem.innerText = userInput.value
+      ul.appendChild(listItem);
+      para.innerText ="";
+      userInput.value ="";
+    }
     
+    })
+    ul.addEventListener( "click", () => {
+      if (event.target.parentNode === ul) {
+        event.target.style.textDecoration = 'line-through'
+      }
   })
 
 })
