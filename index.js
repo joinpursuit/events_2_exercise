79c7eb5b6038071f474314c383dd3180591c196d
bsihame <sihamebazi@pursuit.org>
2019-11-15 23:16:10 -0500

    creat html and js file

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   let h1 = document.createElement("h1");
+  let _form = document.createElement("form");
   let ul = document.createElement("ul");
   let userInput = document.createElement("input");
   let submitButton = document.createElement("button");
-  userInput.innerText = "Type a todo"
-  submitButton.innerText = "submit"
+  
+  userInput.placeholder = "Type a todo";
+  submitButton.innerText = "submit";
+  submitButton.id = "button";
+  
 
   h1.innerText = "My To-Dos"
   document.body.appendChild(h1);
-  document.body.appendChild(ul);
-  document.body.appendChild(userInput);
-  document.body.appendChild(submitButton);
+  document.body.appendChild(_form);
+  _form.appendChild(ul);
+  _form.appendChild(userInput);
+  _form.appendChild(submitButton);
+  submitButton.addEventListener("click",()=>{
+    
+  })
 
 })
